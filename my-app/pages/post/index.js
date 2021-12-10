510537736a222ef1bbd197d223590c5df4c35886
bhima2001 <bhimashankar.d19@iiits.in>
2021-12-10 14:58:54 +0530

    1.Form is added for creating a post with no styling. 2.Linked all the parts i.e. showing all the blogs in db and also retrieving data from the form and adding it to the mockdb

---
+++ my-app/pages/post/index.js
@@ -0,0 +1,30 @@
+import "tailwindcss/tailwind.css"
+import Singlepost from "../../components/Singlepost";
+import {useEffect,useState} from "react"
+
+function Post(){
+    const url="http://localhost:8000/posts";
+    const [data,setData]=useState([])
+
+    
+    useEffect(()=>{
+        async function getdata(){
+            const res=await fetch(url);
+            const jsonData=await res.json();
+            setData(jsonData)
+            console.log(jsonData);
+        }
+        getdata();
+    },[])
+    
+
+    return (
+         <div>
+             {
+            data.map((post)=><Singlepost {...post} key={post.id}/>)
+            }
+         </div>
+    )
+}
+
+export default Post;
