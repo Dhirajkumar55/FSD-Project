869dd5e6667f2088f0d299fba0765396ec619e4f
Dhirajkumar55 <dhiruch7029@gmail.com>
2022-04-27 14:42:19 +0530

    added cors

--- my-app/graphql/client/clientSetup.js
+++ my-app/graphql/client/clientSetup.js
@@ -2,7 +2,7 @@ import { ApolloClient,createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'https://fsd-project.vercel.app/',
+    uri: 'https://fsd-project.vercel.app/api/graphqlServer',
 });
 
 const authLink = setContext((_, { headers }) => {
