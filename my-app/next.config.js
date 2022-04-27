38040fa1b998a02bcd0331281ad27f39a286da2b
Dhirajkumar55 <dhiruch7029@gmail.com>
2022-04-27 13:57:05 +0530

    added cors

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -14,18 +14,4 @@ module.exports = {
         MONGODB_URI : "mongodb+srv://proSpaces:proSpaces@prosapces.g6usv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
         APP_SECRET_KEY : "IFYOUSEETHISYOUWILLGOTOHELL",
     },
-    async headers() {
-        return [
-          {
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "*" },
-            ]
-          }
-        ]
-      }
 };
