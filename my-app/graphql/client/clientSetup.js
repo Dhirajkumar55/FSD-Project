7a5e9c2d0852b188c6b717c713587a11ae39b318
Dhirajkumar55 <dhiruch7029@gmail.com>
2022-04-27 14:59:29 +0530

    changed the url path typo

--- my-app/graphql/client/clientSetup.js
+++ my-app/graphql/client/clientSetup.js
@@ -2,7 +2,7 @@ import { ApolloClient,createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3000/api/graphqlServer',
+    uri: 'https://fsd-project.vercel.app/api/graphqlServer',
 });
 
 const authLink = setContext((_, { headers }) => {
